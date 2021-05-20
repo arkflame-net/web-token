a1f0a2db904b9b9b7a58ff539c273a6a078e4c0d
linsaftw <25271111+linsaftw@users.noreply.github.com>
2021-05-20 10:11:09 -0300

    Init resources & Main class

---
+++ src/com/arkflame/bukkittoken/BukkitToken.java
@@ -0,0 +1,10 @@
+package com.arkflame.bukkittoken;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BukkitToken extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        
+    }
+}
