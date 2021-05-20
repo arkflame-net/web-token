7dabf4e87d8af21af325a37f7a4f5678a095dd06
linsaftw <25271111+linsaftw@users.noreply.github.com>
2021-05-20 11:18:20 -0300

    Register commands & sql on enable

--- src/com/arkflame/bukkittoken/BukkitToken.java
+++ src/com/arkflame/bukkittoken/BukkitToken.java
@@ -1,10 +1,30 @@
 package com.arkflame.bukkittoken;
 
+import com.arkflame.bukkittoken.commands.TokenCommandExecutor;
+import com.arkflame.bukkittoken.sql.SQLConnection;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BukkitToken extends JavaPlugin {
+    private static BukkitToken instance;
+    private SQLConnection sqlConnection = null;
+
     @Override
     public void onEnable() {
-        
+        BukkitToken.instance = this;
+        sqlConnection = new SQLConnection();
+
+        getCommand("token").setExecutor(new TokenCommandExecutor(sqlConnection));
+    }
+
+    @Override
+    public void onDisable() {
+        if (sqlConnection != null) {
+            sqlConnection.close();
+        }
+    }
+
+    public static JavaPlugin getInstance() {
+        return BukkitToken.instance;
     }
 }
